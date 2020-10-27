391ee745db488ecefcf74df16fa02845554018be
Allex372 <62441924+Allex372@users.noreply.github.com>
2020-10-27 12:55:27 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,15 @@
+let hellow = 'hello';
+let name = 'owu';
+let domain = 'com';
+let domain1 = 'ua';
+let number1 = 1;
+let number2 = 10;
+let number3 = -999;
+let number4 = 123;
+let number5 = 3.14;
+let number6 = 2.7;
+let number7 = 16;
+let Adult = true;
+let car = false;
+
+console.log(hellow);
